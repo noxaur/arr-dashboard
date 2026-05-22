f2193cb816cb8b88b5f4d2befe4cccbc9a0aa04b
nox <nox.heights@gmail.com>
2026-05-22 21:44:43 +0200

    fix: force-dynamic rendering for pages using motion/react to avoid prerender crash

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,5 +1,7 @@
 import { EventsContent } from "../events-content";
 
+export const dynamic = "force-dynamic";
+
 export default function EventsPage() {
   return <EventsContent />;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { DashboardContent } from "./dashboard-content";
 
+export const dynamic = "force-dynamic";
+
 export default function DashboardPage() {
   return <DashboardContent />;
 }
